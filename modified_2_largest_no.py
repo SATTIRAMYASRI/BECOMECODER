f1afa4aa2f45153c6116201bafc330f4bc459d8f
SATTIRAMYASRI <69569553+SATTIRAMYASRI@users.noreply.github.com>
2021-06-07 23:16:43 +0530

    Add files via upload

---
+++ modified_2_largest_no.py
@@ -0,0 +1,26 @@
+def second_max(data):
+    #max_no=max(data)
+    #for i in data:
+    #    if i==max_no:
+    #        data.remove(i)
+    #print(max(data))
+    data=list(set(data))
+    data.sort()
+    print(data[-2])
+n=int(input())
+data=list(map(int,input().split()))
+second_max(data)
+'''
+i/p:4
+    1 2 3 4
+o/p:3
+==================
+i/p:6
+    1 2 3 2 3
+o/P:2
+==================
+FAILED TEST CASE
+i/p:6
+    1 2 3 2 3 3
+o/p:2
+'''
